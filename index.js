2e2dc527d39454571db212beb76da52024b78e56
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-03 18:31:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays() {
+  console.log("Happy holidays!")
+}
+happyHolidays()
+
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`)
+}
+happyHolidaysTo("everyone")
+
+function happyCustomHolidayTo(holiday, name) {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
+happyCustomHolidayTo('New Year', 'everyone')
+
+function holidayCountdown(days, holiday) {
+  console.log(`It's ${days} days until ${holiday}!`)
+}
+holidayCountdowns(20, "Mother's Day")
